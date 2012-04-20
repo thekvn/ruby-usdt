43512e36e46ed10e7d845060b3e71471a2b9a64f
Kevin Chan <kevin@yinkei.com>
2012-04-20 16:09:13 +0800

    misc fixes

--- ext/usdt/extconf.rb
+++ ext/usdt/extconf.rb
@@ -7,8 +7,3 @@ have_library('usdt')
 have_header('usdt.h')
 
 create_makefile('usdt')
-
-open("Makefile", "ab") do |mf|
-  mf.puts
-  mf.print "libusdt.a:\n\t cd ../libusdt && make && cp *.a usdt.h ../usdt/ \n"
-end
